d9113fd39657627a1fd22222c7b391047244444d
AliNoorkazemi <alinoorkazemi@gmail.com>
2022-06-24 22:55:15 +0430

    Add project files.

---
+++ Course/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace CourseApp.Models
+{
+    public class Student
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id{ get; set; }
+        public int student_id { get; set; }
+
+        public string name { get; set; }
+
+        public float average { get; set; }
+    }
+}
